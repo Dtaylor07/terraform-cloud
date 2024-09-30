5887666241a34bed861d463e582d6c073ee5e9b9
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-09-29 18:59:46 -0700

    init setup

--- OIDC.tf
+++ OIDC.tf
@@ -44,7 +44,8 @@ data "aws_iam_policy_document" "this" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       # Strip `repo:` to normalize for cases where users may prepend it
-      values = [for repo in local.github_repositories : "repo:Dtaylor07/${repo}:${upper(var.environment)}"]
+     # values = [for repo in local.github_repositories : "repo:Dtaylor07/${repo}:${upper(var.environment)}"]
+      values = formatlist("repo:Dtaylor07/%s:environment:%s", local.github_repositories, upper(var.environment))
     }
   }
 }
