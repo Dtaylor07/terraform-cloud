774101f8d6fe340531aaef310271496fb6c7dce2
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-09-29 19:19:21 -0700

    final setup

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
 
 }
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-file"
+    key     = "security/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
