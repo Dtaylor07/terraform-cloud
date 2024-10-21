62aad2155755edce3065fd1d13899faf6f86d763
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-10-20 20:40:36 -0700

    create app

--- lambda.tf
+++ lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
+  function_name = "todo-app"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.handler"
 
