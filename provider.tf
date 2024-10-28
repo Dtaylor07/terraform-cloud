5d6e52d28d55bd749b770e714bbafaa0a14f4d63
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-10-27 21:51:17 -0700

    udpate code

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
   assume_role {
-    role_arn     = "arn:aws:iam::199660179115:role/github-oidc-Role"
+    role_arn     = "var.AWS_ROLE_ARN"
     session_name = "TerraformCloudSession"
   }
 }
