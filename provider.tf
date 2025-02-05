5828c69956e681526540af9479b4d183b828d30a
Dtaylor07 <81515783+Dtaylor07@users.noreply.github.com>
2025-02-04 17:43:49 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket  = "terraform-state-file-latest"
-    key     = "security/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
